113e147be4d2218dbb9dcfabba824679b422568d
AlexeySinushkin <sinushkin_alexey@mail.ru>
2025-01-12 22:45:25 +0800

    Revert "lifetime issue"
    This reverts commit c35788640d10e669209a488a0dda12c8b56dc3f0.

--- stream-splitter/src/client_side_split.rs
+++ stream-splitter/src/client_side_split.rs
@@ -1,49 +1,52 @@
-use std::cell::RefCell;
 use crate::packet::*;
-use crate::{CommonContext, DataStream, Split, READ_START_AWAIT_TIMEOUT};
+use crate::{DataStream, Split, READ_START_AWAIT_TIMEOUT};
 use log::debug;
 use std::net::{Shutdown, TcpStream};
 use std::sync::mpsc::{channel, Receiver, Sender};
 use easy_error::{bail, Error, ResultExt};
 
-pub fn split_client_stream<'a>(client_stream: TcpStream) -> Split {
-    client_stream
+pub fn split_client_stream(client_stream_param: TcpStream) -> Split {
+    client_stream_param
         .set_read_timeout(Some(READ_START_AWAIT_TIMEOUT))
         .expect("Архитектура подразумевает не блокирующий метод чтения");
-    let filler_stream = client_stream
+    let filler_stream = client_stream_param
         .try_clone()
         .expect("Failed to clone TcpStream");
-
     //Если в методе read пришел чужой пакет - перенаправляем его получателю
-    let context = CommonContext::new();
-
+    let (ct_data, cr_data) = channel();
+    let (ct_filler, cr_filler) = channel();
     Split {
-        data_stream: Box::new(ClientDataStream::<'a>::new(client_stream, &context)),
-        filler_stream: Box::new(FillerDataStream::<'a>::new(filler_stream, &context)),
+        data_stream: Box::new(ClientDataStream::new(client_stream_param, cr_data, ct_filler)),
+        filler_stream: Box::new(FillerDataStream::new(filler_stream, cr_filler, ct_data)),
     }
 }
 
-pub struct ClientDataStream<'a>  {
+pub struct ClientDataStream {
     client_stream: TcpStream,
+    //временный буфер в который получаем и заголовок и тело
     temp_buf: Buffer,
-    context: &'a CommonContext
+    cr: Receiver<QueuedPacket>,
+    ct: Sender<QueuedPacket>,
 }
 
-pub struct FillerDataStream<'a>  {
+pub struct FillerDataStream {
     client_stream: TcpStream,
     temp_buf: Buffer,
-    context: &'a CommonContext
+    cr: Receiver<QueuedPacket>,
+    ct: Sender<QueuedPacket>,
 }
 
-impl<'a> ClientDataStream {
+impl ClientDataStream {
     fn new(
         client_stream: TcpStream,
-        context: &'a CommonContext
+        cr: Receiver<QueuedPacket>,
+        ct: Sender<QueuedPacket>,
     ) -> ClientDataStream {
         Self {
             client_stream,
             temp_buf: [0; BUFFER_SIZE],
-            context
+            cr,
+            ct,
         }
     }
 }
@@ -54,10 +57,10 @@ impl DataStream for ClientDataStream {
     }
 
     fn read(&mut self, dst: &mut [u8]) -> Result<usize, Error> {
-       /* if let Some(packet_body) = self.recv() {
+        if let Ok(packet_body) = self.cr.try_recv() {
             dst[..packet_body.len].copy_from_slice(&packet_body.buf[..packet_body.len]);
             return Ok(packet_body.len);
-        }*/
+        }
         if let Some(packet_info) = read_packet(&mut self.temp_buf, &mut self.client_stream)? {
             let ReadPacketInfo {
                 packet_type,
@@ -69,7 +72,8 @@ impl DataStream for ClientDataStream {
             } else if packet_type == TYPE_FILLER {
                 debug!("Получили пакет заполнителя в методе получения данных");
                 let packet_body = QueuedPacket::copy_from(&self.temp_buf[..packet_size]);
-                //self.send(packet_body);
+                self.ct.send(packet_body)
+                    .context("Alive channel")?;
             } else {
                 bail!("Мусор в данных")
             }
@@ -82,15 +86,17 @@ impl DataStream for ClientDataStream {
     }
 }
 
-impl<'a> FillerDataStream {
+impl FillerDataStream {
     fn new(
         client_stream: TcpStream,
-        context: &'a CommonContext
+        cr: Receiver<QueuedPacket>,
+        ct: Sender<QueuedPacket>,
     ) -> FillerDataStream {
         Self {
             client_stream,
             temp_buf: [0; BUFFER_SIZE],
-            context
+            cr,
+            ct,
         }
     }
 }
@@ -101,10 +107,10 @@ impl DataStream for FillerDataStream {
     }
 
     fn read(&mut self, dst: &mut [u8]) -> Result<usize, Error> {
-       /* if let Some(packet_body) = self.recv() {
+        if let Ok(packet_body) = self.cr.try_recv() {
             dst[..packet_body.len].copy_from_slice(&packet_body.buf[..packet_body.len]);
             return Ok(packet_body.len);
-        }*/
+        }
         if let Some(packet_info) = read_packet(&mut self.temp_buf, &mut self.client_stream)? {
             let ReadPacketInfo {
                 packet_type,
@@ -116,7 +122,8 @@ impl DataStream for FillerDataStream {
             } else if packet_type == TYPE_DATA {
                 debug!("Получили пакет данных в методе получения заполнителя");
                 let packet_body = QueuedPacket::copy_from(&self.temp_buf[..packet_size]);
-                //self.send(packet_body);
+                self.ct.send(packet_body)
+                    .context("Alive channel")?;
             } else {
                 bail!( "Мусор в данных")
             }
