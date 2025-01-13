75b843f97422ca054f7c6c265e26b56148b1d139
AlexeySinushkin <sinushkin_alexey@mail.ru>
2025-01-13 13:13:03 +0800

    Rc

--- stream-splitter/src/client_side_split.rs
+++ stream-splitter/src/client_side_split.rs
@@ -1,86 +1,81 @@
+use std::cell::RefCell;
 use crate::packet::*;
-use crate::{DataStream, READ_START_AWAIT_TIMEOUT};
+use crate::{READ_START_AWAIT_TIMEOUT};
 use log::debug;
 use std::net::{Shutdown, TcpStream};
-use std::sync::mpsc::{channel, Receiver, Sender};
+use std::rc::Rc;
 use easy_error::{bail, Error};
 
+
 pub struct ClientSideSplit {
-    pub data_stream: Box<dyn DataStream>,
-    pub filler_stream: Box<dyn DataStream>,
+    pub data_stream: Rc<dyn DataStreamVpn>,
+    pub filler_stream: Rc<dyn DataStreamFiller>,
 }
 
+pub trait DataStreamVpn {
+    fn write_all(&self, buf: &[u8]) -> Result<(), Error>;
+    fn read(&self, buf: &mut [u8]) -> Result<usize, Error>;
+    fn shutdown(&self);
+}
+pub trait DataStreamFiller {
+    fn write_all(&self, buf: &[u8]) -> Result<(), Error>;
+    fn read(&self, buf: &mut [u8]) -> Result<usize, Error>;
+    fn shutdown(&self);
+}
 
 
 pub fn split_client_stream(client_stream: TcpStream) -> ClientSideSplit {
     client_stream
         .set_read_timeout(Some(READ_START_AWAIT_TIMEOUT))
         .expect("Архитектура подразумевает не блокирующий метод чтения");
-    let filler_stream = client_stream
-        .try_clone()
-        .expect("Failed to clone TcpStream");
-    //Если в методе read пришел чужой пакет - перенаправляем его получателю
-
-    let (ct_data, cr_data) = channel();
-    let (ct_filler, cr_filler) = channel();
+    let ds = Rc::new(CommonDataStream::new(client_stream));
     ClientSideSplit {
-        data_stream: Box::new(ClientDataStream::new(client_stream, cr_data, ct_filler)),
-        filler_stream: Box::new(FillerDataStream::new(filler_stream, cr_filler, ct_data)),
+        data_stream: ds.clone(),
+        filler_stream: ds,
     }
 }
 
-pub struct ClientDataStream  {
-    client_stream: TcpStream,
+struct CommonDataStream  {
+    pub client_stream: RefCell<TcpStream>,
     //временный буфер в который получаем и заголовок и тело
-    temp_buf: Buffer,
-    cr: Receiver<QueuedPacket>,
-    ct: Sender<QueuedPacket>,
+    pub temp_buf: RefCell<Buffer>,
 }
 
-pub struct FillerDataStream  {
-    client_stream: TcpStream,
-    temp_buf: Buffer,
-    cr: Receiver<QueuedPacket>,
-    ct: Sender<QueuedPacket>,
-}
 
-impl ClientDataStream {
+
+impl CommonDataStream {
     fn new(
         client_stream: TcpStream,
-        cr: Receiver<QueuedPacket>,
-        ct: Sender<QueuedPacket>,
-    ) -> ClientDataStream {
+    ) -> CommonDataStream {
         Self {
-            client_stream,
-            temp_buf: [0; BUFFER_SIZE],
-            cr,
-            ct,
+            client_stream : RefCell::new(client_stream),
+            temp_buf: RefCell::new([0; BUFFER_SIZE]),
         }
     }
-}
-
-impl DataStream for ClientDataStream {
-    fn write_all(&mut self, buf: &[u8]) -> Result<(), Error> {
-        write_packet(buf, TYPE_DATA, &mut self.client_stream)
+    pub fn write_as_packet(&self, packet_type: u8, buf: &[u8]) -> Result<(), Error> {
+        let stream = &mut *self.client_stream.borrow_mut();
+        write_packet(buf, packet_type, stream)
     }
-
-    fn read(&mut self, dst: &mut [u8]) -> Result<usize, Error> {
-        if let Ok(packet_body) = self.cr.try_recv() {
+    pub fn read_packet(&self, target_type: u8, redirect_type: u8, dst: &mut [u8]) -> Result<usize, Error> {
+        //Если в методе read пришел чужой пакет - перенаправляем его получателю
+       /* if let Ok(packet_body) = self.cr.try_recv() {
             dst[..packet_body.len].copy_from_slice(&packet_body.buf[..packet_body.len]);
             return Ok(packet_body.len);
-        }
-        if let Some(packet_info) = read_packet(&mut self.temp_buf, &mut self.client_stream)? {
+        }*/
+        let stream = &mut *self.client_stream.borrow_mut();
+        let temp_buf = &mut *self.temp_buf.borrow_mut();
+        if let Some(packet_info) = read_packet(temp_buf, stream)? {
             let ReadPacketInfo {
                 packet_type,
                 packet_size,
             } = packet_info;
-            if packet_type == TYPE_DATA {
-                dst[..packet_size].copy_from_slice(&self.temp_buf[..packet_size]);
+            if packet_type == target_type {
+                dst[..packet_size].copy_from_slice(&temp_buf[..packet_size]);
                 return Ok(packet_info.packet_size);
-            } else if packet_type == TYPE_FILLER {
+            } else if packet_type == redirect_type {
                 debug!("Получили пакет заполнителя в методе получения данных");
-                let packet_body = QueuedPacket::copy_from(&self.temp_buf[..packet_size]);
-                self.ct.send(packet_body).expect("enqueue filler packet");
+                let packet_body = QueuedPacket::copy_from(&temp_buf[..packet_size]);
+                //self.ct.send(packet_body).expect("enqueue filler packet");
             } else {
                 bail!("Мусор в данных")
             }
@@ -88,56 +83,36 @@ impl DataStream for ClientDataStream {
         Ok(0)
     }
 
-    fn shutdown(&mut self) {
-        let _ = self.client_stream.shutdown(Shutdown::Both);
+    pub fn shutdown(&self) {
+        let _ = self.client_stream.borrow_mut().shutdown(Shutdown::Both);
     }
 }
 
-impl FillerDataStream {
-    fn new(
-        client_stream: TcpStream,
-        cr: Receiver<QueuedPacket>,
-        ct: Sender<QueuedPacket>,
-    ) -> FillerDataStream {
-        Self {
-            client_stream,
-            temp_buf: [0; BUFFER_SIZE],
-            cr,
-            ct,
-        }
+impl DataStreamVpn for CommonDataStream {
+    fn write_all(&self, buf: &[u8]) -> Result<(), Error> {
+        self.write_as_packet(TYPE_DATA, buf)
+    }
+
+    fn read(&self, buf: &mut [u8]) -> Result<usize, Error>{
+       self.read_packet(TYPE_DATA, TYPE_FILLER, buf)
+    }
+
+    fn shutdown(&self) {
+        self.shutdown();
     }
 }
 
-impl DataStream for FillerDataStream {
-    fn write_all(&mut self, _buf: &[u8]) -> Result<(), Error> {
-        bail!("Клиент не должен отправлять данных заполнения");
+
+impl DataStreamFiller for CommonDataStream {
+    fn write_all(&self, buf: &[u8]) -> Result<(), Error> {
+        self.write_as_packet(TYPE_FILLER, buf)
     }
 
-    fn read(&mut self, dst: &mut [u8]) -> Result<usize, Error> {
-        if let Ok(packet_body) = self.cr.try_recv() {
-            dst[..packet_body.len].copy_from_slice(&packet_body.buf[..packet_body.len]);
-            return Ok(packet_body.len);
-        }
-        if let Some(packet_info) = read_packet(&mut self.temp_buf, &mut self.client_stream)? {
-            let ReadPacketInfo {
-                packet_type,
-                packet_size,
-            } = packet_info;
-            if packet_type == TYPE_FILLER {
-                dst[..packet_size].copy_from_slice(&self.temp_buf[..packet_size]);
-                return Ok(packet_info.packet_size);
-            } else if packet_type == TYPE_DATA {
-                debug!("Получили пакет данных в методе получения заполнителя");
-                let packet_body = QueuedPacket::copy_from(&self.temp_buf[..packet_size]);
-                self.ct.send(packet_body).expect("enqueue data packet");
-            } else {
-                bail!( "Мусор в данных")
-            }
-        }
-        Ok(0)
+    fn read(&self, buf: &mut [u8]) -> Result<usize, Error>{
+        self.read_packet(TYPE_FILLER, TYPE_DATA, buf)
     }
 
-    fn shutdown(&mut self) {
-        let _ = self.client_stream.shutdown(Shutdown::Both);
+    fn shutdown(&self) {
+        self.shutdown();
     }
 }
