4ea6d38fb07065b143c193cd86c2e8f949714370
AlexeySinushkin <alexeysinushkin@gmail.com>
2025-03-05 13:33:43 +0800

    actualize scripts

--- client-c/down.sh
+++ client-c/down.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 PATH=/usr/sbin:/usr/bin:/sbin:/bin
-#VPN_SERVER_IP=
-#CONTROL_PANEL_IP=
-source /root/env.txt
+source /root/env.sh
 WAN_DEVICE=$(uci get network.wan.device)
 nft flush ruleset
 ip rule del fwmark 0x2 table 200
 ip route del default table 200
-ip route del default dev tun0
+ip route del default dev tun1
 iptables -t mangle -D PREROUTING -i br-lan -p udp --dport 1025:65535 -j MARK --set-mark 0x2
-iptables -t nat -D POSTROUTING -o tun0 -j MASQUERADE
-ip route del $VPN_SERVER_IP table main
+iptables -t nat -D POSTROUTING -o tun1 -j MASQUERADE
+ip route del $VPN_BYPASS_IP table main
 ip route del $CONTROL_PANEL_IP table main
 if [[ -f /tmp/default_gateway.txt ]]; then
     # Load the gateway IP from the file
     WAN_GATEWAY=$(cat /tmp/default_gateway.txt)
-    logger "Environment variables: WAN_GATEWAY=$WAN_GATEWAY"
+    logger "Environment variables: WAN_GATEWAY=$WAN_GATEWAY WAN_DEVICE=$WAN_DEVICE"
     ip route add default via $WAN_GATEWAY dev $WAN_DEVICE table main
 fi
+
--- client-c/up.sh
+++ client-c/up.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 #Get the WAN gateway dynamically
 PATH=/usr/sbin:/usr/bin:/sbin:/bin
-#VPN_SERVER_IP=
-#CONTROL_PANEL_IP=
-source /root/env.txt
+source /root/env.sh
 WAN_DEVICE=$(uci get network.wan.device)
 WAN_GATEWAY=$(ip route list | grep default | awk '{print $3}')
 logger "Writing to temp $WAN_GATEWAY"
 echo "$WAN_GATEWAY" > /tmp/default_gateway.txt
-TUN_PTP_GATEWAY=$(ip -4 addr show tun0 | awk '/peer/ {print $4}' | cut -d'/' -f1)
-logger "Environment variables: WAN_GATEWAY=$WAN_GATEWAY WAN_DEVICE=$WAN_DEVICE TUN_PTP_GATEWAY=$TUN_PTP_GATEWAY"
+TUN_PTP_GATEWAY=$(ip -4 addr show tun1 | awk '/peer/ {print $4}' | cut -d'/' -f1)
+logger "Environment variables: VPN_BYPASS_IP=$VPN_BYPASS_IP WAN_GATEWAY=$WAN_GATEWAY WAN_DEVICE=$WAN_DEVICE TUN_PTP_GATEWAY=$TUN_PTP_GATEWAY"
 # Ensure valid WAN gateway
 if [ -n "$WAN_GATEWAY" ]; then    
-    # Add route to bypass tun0 for openvpn server ip
+    nft flush ruleset
+	# Add route to bypass tun1 for openvpn server ip
     logger "step 1"
-    ip route add $VPN_SERVER_IP via $WAN_GATEWAY dev $WAN_DEVICE table main
+    ip route add $VPN_BYPASS_IP via $WAN_GATEWAY dev $WAN_DEVICE table main
     ip route add $CONTROL_PANEL_IP via $WAN_GATEWAY dev $WAN_DEVICE table main
     logger "step 2"
     nft add table ip nat
     nft add chain ip nat postrouting { type nat hook postrouting priority srcnat \; }
-    nft add rule ip nat postrouting oif tun0 masquerade
+    nft add rule ip nat postrouting oif tun1 masquerade
     logger "step 3"
-    iptables -t mangle -A PREROUTING -i br-lan -p udp --dport 1025:65535 -j MARK --set-mark 0x2
+    #iptables -t mangle -A PREROUTING -i br-lan -p udp --dport 1025:65535 -j MARK --set-mark 0x2
+    nft add table ip mangle
+    nft add chain ip mangle prerouting { type filter hook prerouting priority -150 \; }
+    nft add rule ip mangle prerouting iifname "br-lan" udp dport 1025-65535 mark set 0x2
     ip rule add fwmark 0x2 table 200  # eth0.2 Table
     nft add table ip nat
     nft add chain ip nat postrouting { type nat hook postrouting priority srcnat \; }
@@ -29,15 +31,8 @@ if [ -n "$WAN_GATEWAY" ]; then
     logger "step 4"
     ip route add default via $WAN_GATEWAY dev $WAN_DEVICE table 200
     ip route del default table main
-    ip route add default via $TUN_PTP_GATEWAY dev tun0
-    logger "Bypassing VPN for $VPN_SERVER_IP via $WAN_DEVICE"
+    ip route add default via $TUN_PTP_GATEWAY dev tun1
+    logger "Bypassing VPN for $VPN_BYPASS_IP via $WAN_DEVICE"
 else
     logger "Failed to set bypass route!"
 fi
-
-#ip rule show
-#nft list ruleset
-#ip route show table 200
-#iperf -u -c isp --port 5001
-#iperf -u -c isp --port 53
-#tcpdump -i tun0 -n udp port 5001
