86402f3c910d65ee5db1feb6262447a1042c88d8
AlexeySinushkin <alexeysinushkin@gmail.com>
2025-03-06 12:54:46 +0800

    mt7621 (Xiaomi 4A)

---
+++ client-c/env-mt7621.sh
@@ -0,0 +1,6 @@
+export STAGING_DIR=~/openwrt-mt7621/staging_dir
+export TOOLCHAIN_DIR=$STAGING_DIR/toolchain-mipsel_24kc_gcc-11.2.0_musl
+export TARGET_INCLUDE=$STAGING_DIR/target-mipsel_24kc_musl/usr/include
+export TARGET_LIBS=$STAGING_DIR/target-mipsel_24kc_musl/usr/lib
+export CC=mipsel-openwrt-linux-musl-gcc
+export LD=mipsel-openwrt-linux-musl-ld
