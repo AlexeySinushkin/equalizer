c8679aee2b330ab2acb44c758e52d2f3705f9141
AlexeySinushkin <sinushkin_alexey@mail.ru>
2025-04-05 19:57:13 +0800

    Switch off restore

--- server/src/speed/mod.rs
+++ server/src/speed/mod.rs
@@ -13,10 +13,10 @@ mod packets_logging;
 //pub const INITIAL_SPEED: usize = 1*1024*1024/1000;
 
 //все время шлем данные, чтобы впн-у не пришлось свой keep-alive слать
-pub(crate) const SWITCH_OFF_SPEED : usize = 200 * 1024 / 1000;
+
 //скорость ниже которой мы отключаем филлер (не до жиру - быть бы живу)
-pub(crate) const SHUTDOWN_SPEED : usize = 210 * 1024 / 1000;//Убрать ссылки, инициализировать объекты по-требованию
-pub(crate) const ENABLE_SPEED : usize = 250 * 1024 / 1000;
+pub(crate) const SHUTDOWN_SPEED : usize = 100 * 1024 / 1000;//Убрать ссылки, инициализировать объекты по-требованию
+pub(crate) const ENABLE_SPEED : usize = 150 * 1024 / 1000;
 pub const M_COND: usize = (1024 * 1024 / 10) / 1000;//TODO move
 pub const TO_MB: usize = 1024 * 1024; //TODO move
 pub const TO_KB: usize = 1024;
@@ -24,7 +24,7 @@ const PERCENT_100: usize = 100;
 pub(crate) const LONG_TERM: Duration = Duration::from_secs(3);
 //меняем скорость не чаще этого периода
 pub(crate) const INCREASE_SPEED_PERIOD: Duration = Duration::from_millis(500);
-pub(crate) const DECREASE_SPEED_PERIOD: Duration = Duration::from_secs(2);
+pub(crate) const DECREASE_SPEED_PERIOD: Duration = Duration::from_secs(10);
 
 /*
 Пересчитать байт/мс в Мбит/с
